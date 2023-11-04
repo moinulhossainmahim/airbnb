d80fd19f4f2e1a76841cb0972c59a6e451fcd89d
mhossain2 <mhossain2@tibxtech.com>
2023-11-05 02:19:56 +0600

    added listings basic structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return <EmptyState showReset />
+  }
+
   return (
-    <div className="text-rose-500 text-2xl">Welcome to Airbnb!</div>
+    <Container>
+      <div
+        className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        <div>My future listings</div>
+      </div>
+    </Container>
   )
 }
