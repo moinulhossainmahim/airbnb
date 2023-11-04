f3a10f3dfe92383fba6460dbc28ae8eda0eb77a5
mhossain2 <mhossain2@tibxtech.com>
2023-11-05 01:33:43 +0600

    configure cloudinary and added image upload UI

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -20,6 +20,7 @@ import CountrySelect from '../inputs/CountrySelect';
 import Map from '../Map';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
   CATEGORY = 0,
@@ -65,6 +66,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -199,6 +201,21 @@ const RentModal = () => {
           value={bathroomCount}
           title="Bathrooms"
           subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     )
