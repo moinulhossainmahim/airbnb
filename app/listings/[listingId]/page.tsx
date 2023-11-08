046a13c4fa53c0108968bb1cb729239361e9d86a
mhossain2 <moinulhossainmahim@gmail.com>
2023-11-08 16:14:22 +0600

    added get all reservations and create reservations functionality

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById"
 import Listing from "./Listing";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
@@ -11,11 +12,13 @@ const page = async (
 ) => {
   const listing = await getListingById(params);
   const currentUser = await getCurrentUser();
+  const reservations = await getReservations(params);
 
   return (
     <Listing
       currentUser={currentUser}
       listing={listing}
+      reservations={reservations}
     />
   )
 }
